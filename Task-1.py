168e095b9bc4e6424512e3d815b9211d060f62dd
Tameka Roberson <ttrobersoncreate@gmail.com>
2024-06-25 22:08:38 -0400

    Task 1 Completed

--- Task-1.py
+++ Task-1.py
@@ -1,1 +1,20 @@
+# Exploring Python's OS Module
 
+import os
+
+def list_directory_contents(path):
+    # List and print all files and subdirectories in the given path
+    print(path)
+
+    try:
+        file_list = os.listdir(path)
+    except OSError:
+        return
+    
+    for i in file_list:
+        full_path_display = os.path.join(path, i)
+        print(full_path_display)
+
+
+    
+list_directory_contents() #Used my Code Temple directory to test out the code.
